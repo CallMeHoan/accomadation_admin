d251aaa5760dee4ac14700c7c2e581a5901fe615
ThienHoanNeee <softteam2021@gmail.com>
2022-04-06 10:39:17 +0700

    [Update] set up project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import { Suspense, lazy } from 'react'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import './styles.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				{/* <Switch>
+					<Route component={Homepage} path='/' exact />
+					<Route component={Roompage} path='/rooms' exact />
+				</Switch> */}
+        Hello World
+				<ToastContainer
+					position='top-right'
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+				<ToastContainer />
+			</Suspense>
+		</Router>
     </div>
   );
 }
