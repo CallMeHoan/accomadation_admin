de454eb008ebc9362b2d83898dfadf2527a93906
ThienHoanNeee <softteam2021@gmail.com>
2022-04-07 08:15:08 +0700

    [Update] set up eslint for project

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import { Suspense, lazy } from 'react'
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 import './styles.css'
 
-
 function App() {
-  return (
-    <div className="App">
-      <Router>
-			<Suspense fallback={<h1>Loading...</h1>}>
-				{/* <Switch>
+	return (
+		<div className='App'>
+			<Router>
+				<Suspense fallback={<h1>Loading...</h1>}>
+					{/* <Switch>
 					<Route component={Homepage} path='/' exact />
 					<Route component={Roompage} path='/rooms' exact />
 				</Switch> */}
-        Hello World
-				<ToastContainer
-					position='top-right'
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-				/>
-				<ToastContainer />
-			</Suspense>
-		</Router>
-    </div>
-  );
+					Hello World
+					<ToastContainer
+						position='top-right'
+						autoClose={5000}
+						hideProgressBar={false}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+						pauseOnFocusLoss
+						draggable
+						pauseOnHover
+					/>
+					<ToastContainer />
+				</Suspense>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
