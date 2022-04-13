604746eb26c18834edd3869de79592eacfbe36c6
khoatran1711 <ngavaquan123@gmail.com>
2022-04-13 10:12:17 +0700

    [update] add new_acc page

--- src/components/NewCard/index.js
+++ src/components/NewCard/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './styles.css'
-import image1 from '../../assets/hotels/hotel_room.png'
+import logo from '../../assets/logo/logo_new.png'
 import { Link } from 'react-router-dom'
 import { usePostAccomadation } from '../../queries/useAccomadation.queries'
 
@@ -21,58 +21,69 @@ const NewAccomadattion = () => {
 	})
 	return (
 		<div className='DetailAccomadation'>
+			<section className='logo'>
+				<img src={logo}></img>
+			</section>
+			<section className='title'> ADD A NEW ACCOMADATION INTO OUR LIST</section>
 			<section className='info_container'>
-				<input
-					type='text'
-					className='info_name'
-					onChange={(e) => setInfo({ ...info, name: e.target.value })}
-				/>
-				<div className='info_pictiure'>
-					<input type='text' onChange={(e) => setInfo({ ...info, image_Url: e.target.value })} />
-				</div>
-				<input
-					type='text'
-					className='info_title'
-					onChange={(e) => setInfo({ ...info, title: e.target.value })}
-				/>
-				<input
-					type='text'
-					className='info_description'
-					onChange={(e) => setInfo({ ...info, description: e.target.value })}
-				/>
 				<div className='info_details_book'>
 					<div className='info_details'>
 						<div className='all_details'>
+							<h2>Accomadation's name: </h2>
+							<input
+								type='text'
+								className='detail'
+								onChange={(e) => setInfo({ ...info, name: e.target.value })}
+							/>
+							<h2>Accomadation's Title: </h2>
+							<input
+								type='text'
+								className='detail'
+								onChange={(e) => setInfo({ ...info, title: e.target.value })}
+							/>
+							<h2>Picture Url: </h2>
+							<input
+								type='text'
+								className='detail'
+								onChange={(e) => setInfo({ ...info, image_Url: e.target.value })}
+							/>
+							<h2>Number of Toilets: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, toilets: e.target.value })}
 							/>
+							<h2>Wifi's access: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, wifi: e.target.value })}
 							/>
+							<h2>Number of floors: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, floor: e.target.value })}
 							/>
+							<h2>Number of people: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, people: e.target.value })}
 							/>
+							<h2>Area: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, area: e.target.value })}
 							/>
+							<h2>Description: </h2>
 							<input
 								type='text'
 								className='detail'
 								onChange={(e) => setInfo({ ...info, descrpriceiption: e.target.value })}
 							/>
+							<h2>Available or not: </h2>
 							<input
 								type='text'
 								className='detail'
@@ -94,25 +105,6 @@ const NewAccomadattion = () => {
 							<Link className='link' to='/all-accomadations'>
 								All Accomadations
 							</Link>
-						</div>
-					</div>
-				</div>
-				<div className='Gallary_place'>
-					<div className='Gallary_title'>Gallary</div>
-					<div className='Gallary_picture'>
-						<div className='pictures'>
-							<div className='picture_container'>
-								<img src={info.image_Url}></img>
-							</div>
-							<div className='picture_container'>
-								<img src={info.image_Url}></img>
-							</div>
-							<div className='picture_container'>
-								<img src={info.image_Url}></img>
-							</div>
-							<div className='picture_container'>
-								<img src={info.image_Url}></img>
-							</div>
 						</div>
 					</div>
 				</div>
